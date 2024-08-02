4a72c0dc8ef94ccf28268c56285b9dd8caf46bcf
Yuliia Ivaniuk <ivanykjula@gmail.com>
2024-08-02 17:59:49 +0300

    Generate pdf files according to txt file names

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import pandas
 import glob
 from fpdf import FPDF
 from pathlib import Path
+
+
+filepaths = glob.glob("texts/*.txt")
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+for filepath in filepaths:
+    pdf.add_page()
+    pdf.set_font(family="Times", size=16, style="B")
+    print(filepath)
+    filename = Path(filepath).stem
+    pdf.cell(w=50, h=8, txt=filename.title())
+    print(filename)
+    print(type(filename))
+    pdf.output(f"PDFs/{filename}.pdf")
